108ba096971ef8e6fd22779fe97fe6efc370a5e2
Tyler the Pug <50224819+tylerwhardy@users.noreply.github.com>
2025-07-16 10:45:07 -0400

    Update hives.ps1

--- hives.ps1
+++ hives.ps1
@@ -1,29 +1,26 @@
-#-------------------------------------------------------------------
-# Copy-BypassOpen.ps1
-#-------------------------------------------------------------------
 param(
-  [string]$SourceHive,    # e.g. 'C:\Windows\System32\config\SAM'
-  [string]$DestHive,      # e.g. 'C:\Users\DSU\Documents\SAM.hiv'
-  [int]   $BufferSize = 4MB
+    [string] $SourceHive,
+    [string] $DestHive,
+    [int]    $BufferSize = 4MB
 )
 
-# open the locked file with the "backup semantics" flag
+# Open the locked hive using BackupSemantics
 $inStream = [System.IO.FileStream]::new(
     $SourceHive,
-    [System.IO.FileMode]     ::Open,
+    [System.IO.FileMode]      ::Open,
     [System.IO.FileAccess]    ::Read,
     [System.IO.FileShare]     ::ReadWrite,
     $BufferSize,
     [System.IO.FileOptions]   ::BackupSemantics
 )
 
-# create the output file
+# Create the destination file
 $outStream = [System.IO.File]::Create($DestHive)
 
-# copy
+# Copy the contents
 $inStream.CopyTo($outStream)
 
-# clean up
+# Clean up
 $inStream.Close()
 $outStream.Close()
 
